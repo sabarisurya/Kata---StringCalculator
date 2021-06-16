54e30006104866f6d5f3b8e857a4e41079b87faf
sabarisurya <sabarisurya98@gmail.com>
2021-06-16 14:08:48 +0530

    Task-3-New-Line-("n")-as-delimiter
    StringCalculator.java:
    public int add(String numbers) function is updated to handle ("\n") - newline as a delimiter.
    
    StringCalculatorTests.java:
    New Testcase public void task_3_doubleDelimiter() is added to check if the add() function works fine for the inputs that contains both "," and "\n" as delimiters.

--- StringCalculatorTests.java
+++ StringCalculatorTests.java
@@ -13,8 +13,9 @@ public class StringCalculatorTests {
 	}
 	
 	@Test
-	public void Task_1_2_testAdd()
+	public void task_1_2_testAdd()
 	{
+		System.out.println("\nTest-1\n");
 		assertEquals(0,ob.add(""));
 		assertEquals(32,ob.add("32"));
 		assertEquals(3,ob.add("1,2"));
@@ -22,5 +23,19 @@ public class StringCalculatorTests {
 		assertEquals(0,ob.add(",2"));
 		assertEquals(0,ob.add("20,"));
 		assertEquals(0,ob.add(","));
+	}
+	
+	@Test
+	public void task_3_doubleDelimiter()
+	{
+		System.out.println("\nTest-2\n");
+		assertEquals(0,ob.add("\n"));
+		assertEquals(0,ob.add(","));
+		assertEquals(0,ob.add("\n2"));
+		assertEquals(0,ob.add("2\n"));
+		assertEquals(3,ob.add("1\n2"));
+		assertEquals(6,ob.add("1\n2,3"));
+		assertEquals(31,ob.add("1,2\n3,4,5,6\n10"));
+		
 	}
 }
